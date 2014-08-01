07bdc74b871355ae326f133ae1094925d0d7ee05
Seulkiming <conductor@tutti.kr>
2014-08-02 00:14:07 +0900

    ~5.4 step 2

--- flaskr.py
+++ flaskr.py
@@ -1,23 +1,25 @@
 import os
 import sqlite3
-from flask import Flask, request, sesson, g, redirect, url_fol, abort, render_template, flash
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-#The Config object works similar to a dictionary so we can update it with new values.
+#The Config object works similar to a dictionary
+#so we can update it with new values.
 app.config.update(dict(
-	DATABASE=os.path.join(app.root_path, "flaskr.db"),
-	DEBUG=True,
-	SECRET_KEY="birth:901120",
-	USERNAME="Seulkiming",
-	PASSWORD="default"))
+    DATABASE=os.path.join(app.root_path, "flaskr.db"),
+    DEBUG=True,
+    SECRET_KEY="birth:901120",
+    USERNAME="Seulkiming",
+    PASSWORD="default"))
 app.config.from_envvar("FLASKR_SETTINGS", silent=True)
 
+
 def connect_db():
-	rv = sqlite3.connect(app.config["DATABASE"])
-	rv.row_factory = sqlite3.Row
-	return rv
+    rv = sqlite3.connect(app.config["DATABASE"])
+    rv.row_factory = sqlite3.Row
+    return rv
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
